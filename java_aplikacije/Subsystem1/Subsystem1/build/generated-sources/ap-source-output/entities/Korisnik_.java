34ef99a24d4e987a5804c9a0a5685ff772637937
remaxsrb <remaxsrb@protonmail.com>
2023-02-08 01:23:00 +0100

    ispravno konfigurisan projekat - pokrecu se podsistemi

--- java_aplikacije/Subsystem1/Subsystem1/build/generated-sources/ap-source-output/entities/Korisnik_.java
+++ java_aplikacije/Subsystem1/Subsystem1/build/generated-sources/ap-source-output/entities/Korisnik_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2023-02-06T15:32:18")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2023-02-08T01:11:28")
 @StaticMetamodel(Korisnik.class)
 public class Korisnik_ { 
 
